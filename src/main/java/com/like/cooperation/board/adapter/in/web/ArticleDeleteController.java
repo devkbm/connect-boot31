f7d37a0d9c2e4c141cd86a460548a5fed16f07e8
devkbm <kbm0417@nate.com>
2023-09-07 18:30:04 +0900

    2023-09-07

--- src/main/java/com/like/cooperation/board/adapter/in/web/ArticleDeleteController.java
+++ src/main/java/com/like/cooperation/board/adapter/in/web/ArticleDeleteController.java
@@ -2,27 +2,20 @@ package com.like.cooperation.board.adapter.in.web;
 
 import static com.like.system.core.web.util.ResponseEntityUtil.toList;
 
-import java.util.List;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import com.like.cooperation.board.application.port.in.ArticleDeleteUseCase;
-import com.like.cooperation.board.application.service.ArticleCommandService;
-import com.like.cooperation.board.domain.Article;
 import com.like.system.core.message.MessageUtil;
 
 @Controller
 public class ArticleDeleteController {	
-		
-	private ArticleCommandService service;
+			
 	ArticleDeleteUseCase useCase;
 		
-	public ArticleDeleteController(ArticleCommandService service, ArticleDeleteUseCase useCase) {
-		this.service = service;		
+	public ArticleDeleteController(ArticleDeleteUseCase useCase) {		
 		this.useCase = useCase;
 	}	
 	/*
@@ -42,13 +35,14 @@ public class ArticleDeleteController {
 		
 		return toList(null, MessageUtil.getDeleteMessage(1));
 	}
-	
+	/*
 	@DeleteMapping("/api/grw/board/article")
 	public ResponseEntity<?> deleteArticle(@RequestBody List<Article> articleList) {						
 		
 		service.deleteArticle(articleList);									
 		
 		return toList(null, MessageUtil.getDeleteMessage(articleList.size()));
-	}			
+	}
+	*/			
 	
 }
